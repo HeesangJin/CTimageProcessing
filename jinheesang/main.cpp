f1102c78b08cf389eb35042d512182e72800ef72
HeesangJin <jinheesang@naver.com>
2017-07-10 14:18:22 -0700

    f(x)표시하기 and 3d를 vector<2d>로 표현

--- jinheesang/main.cpp
+++ jinheesang/main.cpp
@@ -7,65 +7,95 @@
 //
 
 #include <iostream>
+#include <vector>
 #include "opencv2/opencv.hpp"
 
+#define NUM_PLANES 3
+#define EPSILON_D 0.4
+
 using namespace std;
-int main(int argc, const char * argv[])
-{
-    string filename = "images/0001.tiff";
-    string windowName = "Hello OpenCV";
-    cv::Mat mat, input;
+
+void readNextInput(int &curFileNum, cv::Mat &mat);
+
+void readNextInput(int &curFileNum, cv::Mat &mat){
+    curFileNum += 1;
+    char curFileName[5];
+    sprintf(curFileName, "%04d", curFileNum);
+    string curFileStr(curFileName);
     
-    cout << "Hello, OpenCV!" << endl;
+    // get next readed filename
+    string filename = "images/" + curFileStr + ".tiff";
     cout << "Opening image = " << filename << endl;
     
+    cv::Mat input;
+    
     //read image
-    mat = cv::imread(filename, 0);
-
+    input = cv::imread(filename, 0);
+    
     //convert uchar to float
-    mat.convertTo(input, CV_32FC1);
+    input.convertTo(mat, CV_32FC1);
     
     cout << "Image dimensions = " << mat.size() << endl;
-    
+    //cout << rows << " ," << cols << endl;
+
+}
+
+void normalizeMat2d(cv::Mat &mat);
+void normalizeMat2d(cv::Mat &mat){
     int rows = mat.rows;
     int cols = mat.cols;
-    
-    cout << rows << " ," << cols << endl;
-    
-    //set of d in 32x32 cube map.
-    //Take all the points above the x-axis and make them into unit vectors.
-    
-    
-    //make 3d points
-    int dims[] = {1010, 988, 990};
-    cv::Mat mnd(3, dims, CV_64F);
-    
-    
     //normalization (0~1)
     for(int i=0; i<cols; i++){
         for(int j=0; j<rows; j++){
-            input.at<float>(i,j) /= 255;
+            mat.at<float>(i,j) /= 255;
         }
     }
-    
+}
+
+void calculFx(cv::Mat &mat, cv::Mat &matFx);
+void calculFx(cv::Mat &mat, cv::Mat &matFx){
+    int rows = mat.rows;
+    int cols = mat.cols;
     //f(x)
     for(int i=0; i<cols; i++){
         for(int j=0; j<rows; j++){
-            if(input.at<float>(i,j) >= 0.4){
-                input.at<float>(i,j) = 0;
+            if(mat.at<float>(i,j) >= EPSILON_D){
+                matFx.at<float>(i,j) = 0;
             }
             else
-                input.at<float>(i,j) = 1;
+                matFx.at<float>(i,j) = 1;
         }
     }
-    //cout << mat.at<uchar>(500,500) << endl;
+}
+
+int main(int argc, const char * argv[]){
+    string windowName = "Hello OpenCV";
+    cv::Mat mat, matFx;
+    vector<cv::Mat> mat3d;
+    vector<cv::Mat> mat3dFx;
+    cout << "Hello, OpenCV!" << endl;
     
-    // show image in cv window
-    cv::imshow(windowName, input);
-    
-    // wait for keypress to exit
-    cv::waitKey(0);
-    
+    int curFileNum = 0;
+    while(curFileNum < NUM_PLANES){
+        //read input image
+        readNextInput(curFileNum, mat);
+        
+        //make mat2d to normalization
+        normalizeMat2d(mat);
+        mat3d.push_back(mat);
+        
+        //caculate f(x)
+        matFx = cv::Mat(mat.rows,mat.cols, CV_32FC1, float(0));
+        calculFx(mat, matFx);
+        mat3dFx.push_back(matFx);
+        
+        // show image in cv window
+        // input: original, mat: f(x)
+        cv::imshow(windowName, matFx);
+        
+        // wait for keypress to exit
+        cv::waitKey(0);
+    }
     return 0;
 }
 
