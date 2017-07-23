b2ddec5b115133fba7bbf52fdd175e3cd3a15dbc
HeesangJin <jinheesang@naver.com>
2017-07-23 16:56:26 -0700

    VOL파일 int까진 읽는데 성공함

--- jinheesang/read_vol.cpp
+++ jinheesang/read_vol.cpp
@@ -5,7 +5,7 @@ int main(void)
 {
     FILE    *fp_sour;
     FILE    *fp_dest;
-    char     buff[2048];
+    unsigned char buff[2048];
     size_t   n_size;
     char c;
     
@@ -16,40 +16,68 @@ int main(void)
 //        printf("%d: %d\n", i+1 ,buff[i]);
 //    }
     int i=0;
-    printf("%d: %d\n", i+1 ,buff[i++]);
-    printf("%d: %d\n", i+1 ,buff[i++]);
-    printf("%d: %d\n", i+1 ,buff[i++]);
-    printf("%d: %d\n", i+1 ,buff[i++]);
+    printf("%d: %c\n", i+1 ,buff[i]);
+    i++;
+    printf("%d: %c\n", i+1 ,buff[i]);
+    i++;
+    printf("%d: %c\n", i+1 ,buff[i]);
+    i++;
+    printf("%d: %d\n", i+1 ,buff[i]);
+    i++;
     
-    int32_t final;
-    
-    final=0;
-    final |= ( buff[i++] << 24 );
-    final |= ( buff[i++] << 16 );
-    final |= ( buff[i++] <<  8 );
-    final |= ( buff[i++]       );
-    printf("%d: %d\n", i+1 ,final);
+    int final;
     
     final=0;
-    final |= ( buff[i++] << 24 );
-    final |= ( buff[i++] << 16 );
-    final |= ( buff[i++] <<  8 );
-    final |= ( buff[i++]       );
+    final |= ( (int)(buff[i++])  );
+    final |= ( (int)(buff[i++]) << 8 );
+    final |= ( (int)(buff[i++]) <<  16 );
+    final |= ( (int)(buff[i++]) <<  24  );
     printf("%d: %d\n", i+1 ,final);
     
-    final=0;
-    final |= ( buff[i++] << 24 );
-    final |= ( buff[i++] << 16 );
-    final |= ( buff[i++] <<  8 );
-    final |= ( buff[i++]       );
-    printf("%d: %d\n", i+1 ,final);
     
     final=0;
-    final |= ( buff[i++] << 24 );
-    final |= ( buff[i++] << 16 );
-    final |= ( buff[i++] <<  8 );
-    final |= ( buff[i++]       );
-    printf("%d: %d\n", i+1 ,final);
+    final |= ( (int)(buff[i++])  );
+    final |= ( (int)(buff[i++]) << 8 );
+    final |= ( (int)(buff[i++]) <<  16 );
+    final |= ( (int)(buff[i++]) <<  24  );
+    printf("number of x: %d\n", final);
+    
+    
+    final=0;
+    final |= ( (int)(buff[i++])  );
+    final |= ( (int)(buff[i++]) << 8 );
+    final |= ( (int)(buff[i++]) <<  16 );
+    final |= ( (int)(buff[i++]) <<  24  );
+    printf("number of y: %d\n", final);
+    
+    final=0;
+    final |= ( (int)(buff[i++])  );
+    final |= ( (int)(buff[i++]) << 8 );
+    final |= ( (int)(buff[i++]) <<  16 );
+    final |= ( (int)(buff[i++]) <<  24  );
+    printf("number of z: %d\n", final);
+    
+    final=0;
+    final |= ( (int)(buff[i++])  );
+    final |= ( (int)(buff[i++]) << 8 );
+    final |= ( (int)(buff[i++]) <<  16 );
+    final |= ( (int)(buff[i++]) <<  24  );
+    printf("number of channels: %d\n", final);
+    
+    ////////////////////////////////////////////////////////
+    
+    int floatingNum=0;
+    printf("[%d]\n", buff[i]);
+    floatingNum |= ( buff[i++]  );
+    printf("[%d]\n", buff[i]);
+    floatingNum |= ( buff[i++] << 8 );
+    printf("[%d]\n", buff[i]);
+    floatingNum |= ( buff[i++] <<  16 );
+    printf("[%d]\n", buff[i]);
+    floatingNum |= ( buff[i++] <<  24  );
+    printf("xmin: %f\n", floatingNum);
+    
+    
     
     fclose(fp_sour);
     return 0;
