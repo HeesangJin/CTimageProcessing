ad8fd171ab867d098120d47ab2447414a30e3cd4
HyeJi Kim <137373@gmail.com>
2017-08-08 20:57:23 -0700

    add trackbar

--- kimhyeji/recoverOrient.cpp
+++ kimhyeji/recoverOrient.cpp
@@ -11,7 +11,7 @@
 #define H 13 // H + 1
 #define DIRNUM 12
 #define DIRNUMSQU 122 // DIRNUM * DIRNUM - (2 * DIRNUM-1)
-#define JTHRESHOLD 0.001
+#define JTHRESHOLD -8.3
 using namespace cv;
 using namespace std;
 
@@ -83,21 +83,24 @@ void makeVolume(vector<Mat> &v) {
 /*
 get rotation matrix
 z,y,x
+using spherical coordinate angles(theta, delta)
+t = 2PAI - theta
+d = delta - 1/2*PAI 
 */
-Mat rotationMatrix(float d, float cosTheta) {
-	float sinTheta = sin(acos(cosTheta));
+Mat rotationMatrix(float d, float cosT) {
+	float sinT = sin(acos(cosT));
 
-	Mat YR = (Mat_<float>(3, 3) <<
-		cosTheta,0, -sinTheta,
+	Mat XR = (Mat_<float>(3, 3) <<
+		cosT,0, -sinT,
 		0, 1, 0,
-		sinTheta, 0, cosTheta
+		sinT, 0, cosT
 		);
 	Mat ZR = (Mat_<float>(3, 3) <<
 		1, 0, 0,
 		0, sin(d), cos(d),
 		0, -cos(d), sin(d)
 		);
-	return ZR*YR;
+	return ZR*XR;
 }
 
 /*
@@ -148,9 +151,9 @@ void saveDistance(float(&saveData)[H*H*H], const directionSet &d, vector<Mat> &R
 }
 
 /*
-get Maximum convolution value
+testing code for making J value images.
 */
-Vec3f getMaxConvolution(const directionSet &d, const vector<Mat> &volume, const Vec3i &v, const float(&q)[H * H * H], bool density, vector<Mat> &R) {
+float getJvalue(const directionSet &d, const vector<Mat> &volume, const Vec3i &v, const float(&q)[H * H * H], bool density, vector<Mat> &R) {
 	int z = v[0];
 	int y = v[1];
 	int x = v[2];
@@ -169,12 +172,12 @@ Vec3f getMaxConvolution(const directionSet &d, const vector<Mat> &volume, const 
 					Mat x_vec = (Mat_<float>(3, 1) <<
 						p_z - h_half, p_y - h_half, p_x - h_half);
 					Mat p = R[i] * x_vec;
+					int v_z = int(z + p.at<float>(0, 0) + 0.5);
 					int v_y = int(y + p.at<float>(1, 0) + 0.5);
 					int v_x = int(x + p.at<float>(2, 0) + 0.5);
-					int v_z = int(z + p.at<float>(0, 0) + 0.5);
 
 					if (v_z < 0) continue;
-					if (volume[v_z].at<uchar>(v_x, v_y) > 0) {
+					if (volume[v_z].at<uchar>(v_y, v_x) > 0) {
 						float q_func = q[p_z*h2 + p_y*H + p_x];
 						j += q_func;
 					}
@@ -189,8 +192,109 @@ Vec3f getMaxConvolution(const directionSet &d, const vector<Mat> &volume, const 
 		}
 	}
 
-	density = (maxValue > JTHRESHOLD) ? 1 : 0;
+	density = (maxValue > 0.001) ? 1 : 0;
+	return maxValue;
+}
+
+/*
+get Maximum convolution value
+*/
+Vec3f getMaxConvolution(const directionSet &d, const vector<Mat> &volume, const Vec3i &v, const float(&q)[H * H * H], bool &density, vector<Mat> &R) {
+	int z = v[0];
+	int y = v[1];
+	int x = v[2];
+	float maxValue = -9999;
+	int h_half = H / 2;
+
+	Vec3f result;
+	for (int i = 0; i < DIRNUMSQU; i++) {
+		float j = 0;
+		for (int p_z = 0; p_z < H; p_z++) {
+			if (z + p_z >= IMG_NUM) continue;
+			for (int p_y = 0; p_y < H; p_y++) {
+				if (y + p_y >= ROW) continue;
+				for (int p_x = 0; p_x < H; p_x++) {
+					if (x + p_x >= COL) continue;
+					Mat x_vec = (Mat_<float>(3, 1) <<
+						p_z - h_half, p_y - h_half, p_x - h_half);
+					Mat p = R[i] * x_vec;
+					int v_z = int(z + p.at<float>(0, 0) + 0.5);
+					int v_y = int(y + p.at<float>(1, 0) + 0.5);
+					int v_x = int(x + p.at<float>(2, 0) + 0.5);
+
+					if (v_z < 0) continue;
+					if (volume[v_z].at<uchar>(v_y, v_x) > 0) {
+						float q_func = q[p_z*h2 + p_y*H + p_x];
+						j += q_func;
+					}
+
+				}
+			}
+		}
+		// get maximum of J values
+		if (maxValue < j) {
+			maxValue = j;
+			result = d[i];
+		}
+	}
+
+	if (maxValue < JTHRESHOLD) {
+		return Vec3f(0, 0, 0);
+	}
 	return result;
+}
+
+/*
+check keyboard input,
+change the J threshold
+*/
+void onChange(int pos, void *user){
+
+	Mat image = ((Mat*)user)[0];
+	Mat test(ROW, COL, CV_8UC1);
+	float threshold = pos * -0.1;
+	for (int i = 0; i < 494; i++) {
+		for (int j = 0; j < 505; j++) {
+			if (image.at<float>(i, j) > threshold)
+				test.at<uchar>(i, j) = 255;
+			else test.at<uchar>(i, j) = 0;
+		}
+	}
+	cout << threshold << endl;
+	imshow("image", test);
+}
+
+/*
+set J threshold with ,(-) .(+) keys
+threshold = pos * -0.1
+-8.2
+*/
+void setThreshold(){
+
+	Mat image = imread(fileLocate + "test.exr", CV_LOAD_IMAGE_UNCHANGED);
+	int pos = 100;
+
+	imshow("image", image);
+	onChange(pos, (void *)&image);
+	createTrackbar("threshold", "image", &pos, 255, onChange, (void*)&image);
+	while (1) {
+		char Key = (char)waitKey();
+		if (Key == 27) break;
+		switch (Key) {
+		case ',':
+			pos--;
+			onChange(pos, (void *)&image);
+			setTrackbarPos("threshold","image", pos);
+			break;
+		case '.':
+			pos++;
+			onChange(pos, (void *)&image);
+			setTrackbarPos("threshold", "image", pos);
+			break;
+		}
+		if (pos < 0 || pos > 255) pos = 0;
+	}
+
 }
 
 int main(int argc, char **argv) {
@@ -210,7 +314,7 @@ int main(int argc, char **argv) {
 	for (int z = 30; z < IMG_NUM; z++) {
 		cout << z << endl;
 		Mat image_dir(ROW, COL, CV_32FC3);
-		Mat image_den(ROW, COL, CV_32FC1);
+		//Mat image_den(ROW, COL, CV_32FC1);
 		bool density = 0;
 
 		#pragma omp parallel for schedule(dynamic,6)
@@ -223,30 +327,23 @@ int main(int argc, char **argv) {
 					continue;
 				}
 				Vec3f result = getMaxConvolution(dicVec, volumeData, Vec3i(z, y, x), qfuncData, density, R);
-
-				if (density) {
-					image_dir.at<Vec3f>(y, x)[0] = result[1] * 255;//b - x(row)
-					image_dir.at<Vec3f>(y, x)[1] = result[2] * 255;//g - y(col)
-					image_dir.at<Vec3f>(y, x)[2] = result[0] * 255;//r - z(depth)
-				}
-				else {
-					image_dir.at<Vec3f>(y, x)[0] = 0;
-					image_dir.at<Vec3f>(y, x)[1] = 0;
-					image_dir.at<Vec3f>(y, x)[2] = 0;
-				}
+				
+				image_dir.at<Vec3f>(y, x)[0] = result[2] * 255;//b - x(row)
+				image_dir.at<Vec3f>(y, x)[1] = result[1] * 255;//g - y(col)
+				image_dir.at<Vec3f>(y, x)[2] = result[0] * 255;//r - z(depth)
+								
+				/*
+				J threshold test
+				*/
+				//image_den.at<float>(y,x) = getJvalue(dicVec, volumeData, Vec3i(z, y, x), qfuncData, density, R);
 			}
 
 			if (y % 10 == 0) {
 				cout << y << endl;
-				imwrite("0.001" + format("%d.jpg", z), image_dir);
+				imwrite("-8.3 " + format("%d.jpg", z), image_dir);
 			}
 		}
-		imwrite("direction" + format("%d.tiff", z), image_dir);
-		imwrite("density" + format("%d.tiff", z), image_den);
 		image_dir.release();
-		image_den.release();
 	}
 
 }
-
-
